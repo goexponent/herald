a9fd7fe126a7b9c3e62d7ed4b8e06f33116f4443
destifo <estifanosbireda@gmail.com>
2025-01-04 15:03:23 +0300

    chore: replace brew installs with direct installs

--- ghjk.ts
+++ ghjk.ts
@@ -85,19 +85,11 @@ const ghjk = file({
   "install-sys-deps": {
     desc: "Install system dependencies",
     async fn($) {
-      // FIXME: doesn't work on mac
       // deno
       await $.raw`curl -fsSL https://deno.land/install.sh | sh`;
       // pre-commit
-      await $.raw`curl -fsSL https://raw.githubusercontent.com/pre-commit/pre-commit/main/pre-commit.pyz -o pre-commit`;
-      await $.raw`chmod +x pre-commit`;
-      await $.raw`mv pre-commit /usr/local/bin/pre-commit`;
+      await $.raw`pip install pre-commit`;
       await $.raw`pre-commit install`;
-      // tofu
-      await $.raw`curl --proto '=https' --tlsv1.2 -fsSL https://get.opentofu.org/install-opentofu.sh -o install-opentofu.sh`;
-      await $.raw`chmod +x install-opentofu.sh`;
-      await $.raw`./install-opentofu.sh --install-method deb`;
-      await $.raw`rm -f install-opentofu.sh`;
     }
   }
 
