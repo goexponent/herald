2dae09fb8c99bfae5c04a92dec18902bafca2d86
destifo <estifanosbireda@gmail.com>
2025-01-06 11:34:57 +0300

    chore: address comments

--- src/backends/tasks.ts
+++ src/backends/tasks.ts
@@ -14,19 +14,23 @@ async function setupWorkers() {
       type: "module",
       name: `bucket ${bucket} kv worker`,
     });
-    await new Promise((resolve, reject) => {
-      worker.onmessage = (evt: MessageEvent<string>) => {
-        const res = evt.data;
-        logger.info(res);
-        resolve(res);
-      };
-      worker.onmessageerror = (evt) => {
-        reject(evt.data);
-      };
-      worker.onerror = (err) => {
-        reject(err);
-      };
-    });
+    // Hack to get around the fact that the worker is not yet ready to receive messages
+    const workerCreation = () => {
+      return new Promise((resolve, reject) => {
+        worker.onmessage = (evt: MessageEvent<string>) => {
+          const res = evt.data;
+          logger.info(res);
+          resolve(res);
+        };
+        worker.onmessageerror = (evt) => {
+          reject(evt.data);
+        };
+        worker.onerror = (err) => {
+          reject(err);
+        };
+      });
+    };
+    await workerCreation();
     workers.set(bucket, worker);
   }
 
@@ -54,22 +58,25 @@ export function taskHandler() {
     workers.delete(taskBucket);
 
     worker.postMessage(task);
-    await new Promise((resolve, reject) => {
-      worker.onmessage = (evt: MessageEvent<string>) => {
-        const res = evt.data;
-        if (res === "Task completed") {
-          workers.set(taskBucket, worker);
-          logger.info(`Task completed: ${task.command}`);
-        }
-        resolve(res);
-      };
-      worker.onmessageerror = (evt) => {
-        reject(evt.data);
-      };
-      worker.onerror = (err) => {
-        reject(err);
-      };
-    });
+    const processMessageBackFromWorker = () => {
+      return new Promise((resolve, reject) => {
+        worker.onmessage = (evt: MessageEvent<string>) => {
+          const res = evt.data;
+          if (res === "Task completed") {
+            workers.set(taskBucket, worker);
+            logger.info(`Task completed: ${task.command}`);
+          }
+          resolve(res);
+        };
+        worker.onmessageerror = (evt) => {
+          reject(evt.data);
+        };
+        worker.onerror = (err) => {
+          reject(err);
+        };
+      });
+    };
+    await processMessageBackFromWorker();
   });
 }
 
