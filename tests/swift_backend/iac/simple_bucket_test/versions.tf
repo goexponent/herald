839dd8633d8be9650c2127486a7b61f40e7fcff0
Estifanos Bireda <77430541+destifo@users.noreply.github.com>
2025-01-08 12:30:13 +0300

    feat: prepare github actions (#6)
    * feat: prepare github actions
    
    * chore: remove rusttoolchain step from pre-commit run
    
    * chore: add docker build job
    
    * chore: add tofu to pre-commit job
    
    * fix: tofu install step
    
    * chore: add sys deps install ghjk task
    
    * fix: brew install in ghjk
    
    * fix: brew install in ghjk
    
    * chore: replace brew installs with direct installs
    
    * chore: replace brew installs with direct installs
    
    * fix: outdated ghjk lockfile
    
    * chore: update deno.lock
    
    * chore: install specific deno version on CI
    
    * fix: pre-commit job
    
    * chore: uncomment docker build job
    
    * refactor: remove commented code
    
    * chore: revert herald-compose.yaml

--- tests/swift_backend/iac/simple_bucket_test/versions.tf
+++ tests/swift_backend/iac/simple_bucket_test/versions.tf
@@ -7,7 +7,7 @@ provider "aws" {
     s3 = "http://localhost:8000/"
   }
 
-  region     = "us-east-1"
+  region = "us-east-1"
 
   access_key = "test:tester"
   secret_key = "testing"
@@ -16,6 +16,6 @@ provider "aws" {
   skip_credentials_validation = true
   skip_region_validation      = true
   skip_requesting_account_id  = true
-  s3_use_path_style            = true
+  s3_use_path_style           = true
   # skip_s3_checksum            = true
 }
